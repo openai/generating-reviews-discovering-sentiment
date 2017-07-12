9b8b1ae714583257feca9663b9fb585b0378af4b
OpenAI server <sys@openai.com>
2017-07-12 16:55:09 -0700

    add sentiment unit histogram vis

--- sst_binary_demo.py
+++ sst_binary_demo.py
@@ -1,4 +1,5 @@
 from encoder import Model
+from matplotlib import pyplot as plt
 from utils import sst_binary, train_with_reg_cv
 
 model = Model()
@@ -8,7 +9,15 @@ trXt = model.transform(trX)
 vaXt = model.transform(vaX)
 teXt = model.transform(teX)
 
+# classification results
 full_rep_acc, c, nnotzero = train_with_reg_cv(trXt, trY, vaXt, vaY, teXt, teY)
 print('%05.2f test accuracy'%full_rep_acc)
 print('%05.2f regularization coef'%c)
 print('%05d features used'%nnotzero)
+
+# visualize sentiment unit
+sentiment_unit = trXt[:, 2388]
+plt.hist(sentiment_unit[trY==0], bins=25, alpha=0.5, label='neg')
+plt.hist(sentiment_unit[trY==1], bins=25, alpha=0.5, label='pos')
+plt.legend()
+plt.show()
